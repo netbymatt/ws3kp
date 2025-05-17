63b35c2bec4c93a2788900f07931964656a471b0
Matt Walsh <github@netbymatt.com>
2025-05-16 22:35:34 -0500

    fix s3 region

--- gulp/publish-frontend.mjs
+++ gulp/publish-frontend.mjs
@@ -121,7 +121,7 @@ const copyOtherFiles = () => src(otherFiles, { base: 'server/' })
 
 const s3 = s3Upload({
 	useIAM: true,
-	region: 'us-east-2',
+	region: 'us-east-1',
 });
 const uploadSources = [
 	'dist/**',
